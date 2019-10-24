c5f2540651cda8dceba41e6e0b917103b7e982bd
lawseff <lawseff@gmail.com>
2019-10-24 21:15:58 +0300

    Implement PasswordEncryptor

--- src/main/java/com/epam/booking/utils/PasswordEncryptor.java
+++ src/main/java/com/epam/booking/utils/PasswordEncryptor.java
@@ -1,9 +1,27 @@
 package com.epam.booking.utils;
 
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PasswordEncryptor {
 
+    private static final String ALGORITHM = "MD5";
+
     public static String encryptPassword(String password) {
-        return null;
+        MessageDigest messageDigest;
+        try {
+            messageDigest = MessageDigest.getInstance(ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+
+        byte[] bytes = password.getBytes();
+        messageDigest.update(bytes);
+        byte[] digest = messageDigest.digest();
+
+        return DatatypeConverter.printHexBinary(digest)
+                .toLowerCase();
     }
 
 }
