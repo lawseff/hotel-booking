50b09bd126fb637c9b7f694db37df07ced8d7383
lawseff <lawseff@gmail.com>
2019-10-25 16:18:59 +0300

    Improve card expiration date validation

--- src/main/java/com/epam/booking/validation/impl/PaymentValidatorImpl.java
+++ src/main/java/com/epam/booking/validation/impl/PaymentValidatorImpl.java
@@ -1,12 +1,24 @@
 package com.epam.booking.validation.impl;
 
+import com.epam.booking.utils.DateUtils;
 import com.epam.booking.validation.api.PaymentValidator;
 
 public class PaymentValidatorImpl implements PaymentValidator {
 
+    // card number
     private static final String CARD_NUMBER_REGEX = "\\d{16}";
+    // expiration date
     private static final String EXPIRATION_DATE_REGEX = "(0[1-9]|1[0-2])/\\d{2}";
+    private static final int EXPIRATION_DATE_DELIMITER_INDEX = 2;
+    private static final int BEGIN_YEAR = 2000;
+    // cvv number
     private static final String CVV_NUMBER_REGEX = "\\d{3}";
+
+    private DateUtils dateUtils;
+
+    public PaymentValidatorImpl(DateUtils dateUtils) {
+        this.dateUtils = dateUtils;
+    }
 
     @Override
     public boolean isCardNumberValid(String cardNumber) {
@@ -16,12 +28,30 @@ public class PaymentValidatorImpl implements PaymentValidator {
 
     @Override
     public boolean isExpirationDateValid(String expirationDate) {
-        return expirationDate.matches(EXPIRATION_DATE_REGEX);
+        if (!expirationDate.matches(EXPIRATION_DATE_REGEX)) {
+            return false;
+        }
+        int month = parseMonth(expirationDate);
+        int year = parseYear(expirationDate);
+        int currentMonth = dateUtils.getCurrentMonth();
+        int currentYear = dateUtils.getCurrentYear();
+        return year > currentYear || year == currentYear && month >= currentMonth;
     }
 
     @Override
     public boolean isCvvNumberValid(String cvvNumber) {
         return cvvNumber.matches(CVV_NUMBER_REGEX);
+    }
+
+    private int parseMonth(String expirationDate) {
+        String month = expirationDate.substring(0, EXPIRATION_DATE_DELIMITER_INDEX);
+        return Integer.parseInt(month);
+    }
+
+    private int parseYear(String expirationDate) {
+        int beginIndex = EXPIRATION_DATE_DELIMITER_INDEX + 1;
+        String year = expirationDate.substring(beginIndex);
+        return BEGIN_YEAR + Integer.parseInt(year);
     }
 
 }
