054c6a43682c625a6e7bbf44e1ea0c8c0bcba1ae
lawseff <lawseff@gmail.com>
2019-10-25 09:32:31 +0300

    Implement PaymentValidatorImpl

--- src/main/java/com/epam/booking/validation/impl/PaymentValidatorImpl.java
+++ src/main/java/com/epam/booking/validation/impl/PaymentValidatorImpl.java
@@ -4,21 +4,24 @@ import com.epam.booking.validation.api.PaymentValidator;
 
 public class PaymentValidatorImpl implements PaymentValidator {
 
-    private static final String CARD_NUMBER_PATTERN = "\\d{16}";
+    private static final String CARD_NUMBER_REGEX = "\\d{16}";
+    private static final String EXPIRATION_DATE_REGEX = "(0[1-9]|1[0-2])/\\d{2}";
+    private static final String CVV_NUMBER_REGEX = "\\d{3}";
 
     @Override
     public boolean isCardNumberValid(String cardNumber) {
-        return false;
+        return cardNumber.matches(CARD_NUMBER_REGEX);
+        // TODO Luhn algorithm
     }
 
     @Override
     public boolean isExpirationDateValid(String expirationDate) {
-        return false;
+        return expirationDate.matches(EXPIRATION_DATE_REGEX);
     }
 
     @Override
     public boolean isCvvNumberValid(String cvvNumber) {
-        return false;
+        return cvvNumber.matches(CVV_NUMBER_REGEX);
     }
 
 }
