96054f2db1f07f5a178668b6c84d6673ed7a7d6b
Mikhail Loseu <mikhail.loseu@gmail.com>
2020-05-11 17:28:40 +0300

    Update insert_users.sql

--- src/main/webapp/sql/insert_users.sql
+++ src/main/webapp/sql/insert_users.sql
@@ -3,7 +3,7 @@ USE hotel_booking;
 DELETE FROM booking_user;
 
 INSERT INTO booking_user (is_admin, email, user_password, first_name, second_name)
-	VALUES (true, 'admin', 'admin', 'admin', 'admin');
+	VALUES (true, 'admin', md5('admin'), 'admin', 'admin');
 
 INSERT INTO booking_user (email, user_password, first_name, second_name) 
 	VALUES ('test@foo.bar', md5('1234'), 'John', 'Doe'),
